29d52f28e6b899e752116c88a4a124f4e4d743d4
felix <felix_yu@epam.com>
2021-12-28 23:29:33 +0800

    change registry to kubernetes by default

--- common/mesh/registry.go
+++ common/mesh/registry.go
@@ -1,20 +1,51 @@
 package mesh
 
 import (
+	"os"
+
 	"github.com/FelixAnna/web-service-dlw/common/aws"
 
 	"github.com/asim/go-micro/plugins/registry/consul/v4"
+	"github.com/asim/go-micro/plugins/registry/etcd/v4"
+	"github.com/asim/go-micro/plugins/registry/kubernetes/v4"
 	"go-micro.dev/v4/registry"
 )
 
-var consulRegAddr string
+var (
+	consulRegAddr string
+	etcdRegAddr   string
+	//kubernetesRegAddr string
+)
 
 func init() {
 	consulRegAddr = aws.GetParameterByKey("mesh/consulRegAddr")
+	etcdRegAddr = aws.GetParameterByKey("mesh/etcdRegAddr")
+	//kubernetesRegAddr = aws.GetParameterByKey("mesh/kubernetesRegAddr")
+}
+
+func GetRegistry() registry.Registry {
+	profile := os.Getenv("profile")
+	if profile == "dev" {
+		return GetConsulRegistry()
+	}
+
+	return GetKubernetesRegistry()
 }
 
 func GetConsulRegistry() registry.Registry {
 	consulReg := consul.NewRegistry(registry.Addrs(consulRegAddr))
 
 	return consulReg
+}
+
+func GetEtcdRegistry() registry.Registry {
+	etcdReg := etcd.NewRegistry(registry.Addrs(etcdRegAddr))
+
+	return etcdReg
+}
+
+func GetKubernetesRegistry() registry.Registry {
+	k8sReg := kubernetes.NewRegistry()
+
+	return k8sReg
 }

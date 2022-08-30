1f775b6384aebac22d2813ff00544d8228b15346
FelixAnna <yuecnu@hotmail.com>
2022-08-30 14:26:10 +0800

    update scripts for appgw + aks deployment

--- deployment/kubernetes/aks_appgw_provisioning.sh
+++ deployment/kubernetes/aks_appgw_provisioning.sh
@@ -3,11 +3,7 @@
 # Export-PfxCertificate -cert Cert:\localMachine\my\A5CE8D378ED5B664D61E59FF57C5D874DDF1CF35  -FilePath C:\Users\Felix_Yu\Downloads\testCert.pfx -Password $pwd
   # https://docs.microsoft.com/en-us/azure/application-gateway/tutorial-ssl-cli
 
-## provide certificate
-
-## provisioning application gateway
-echo "provisioning application gateway"
-
+## prepare
 rgName=dlwRG
 region=eastus
 ipName=dlwAppGWIp
@@ -15,6 +11,49 @@ vnetName=appGWVnet
 subnetName=gwSubnet
 appgwName=dlwAppGateway
 clusterName=dlwCluster
+identityName=appgwIdentity
+vaultName=dlwVault
+
+az group create --name $rgName --location $region
+
+## provide certificate
+
+# create user managed identity
+az identity create -n $identityName -g $rgName -l $region
+identityID=$(az identity show -n $identityName -g $rgName -o tsv --query "id")
+identityPrincipal=$(az identity show -n $identityName -g $rgName -o tsv --query "principalId")
+
+# One time operation, assign AGIC identity to have operator access over AppGw identity
+az role assignment create --role "Managed Identity Operator" --assignee $identityPrincipal --scope $identityID
+
+# One time operation, create Azure key vault and certificate (can done through portal as well)
+az keyvault create -n $vaultName -g $rgName -l $region
+
+# One time operation, assign the identity GET secret access to Azure Key Vault
+az keyvault set-policy \
+	-n $vaultName \
+	-g $rgName \
+	--object-id $identityPrincipal \
+	--secret-permissions get
+
+# One time operation, assign the identity GET secret access to Azure Key Vault
+az keyvault set-policy \
+	-n $vaultName \
+	-g $rgName \
+	--object-id $identityPrincipal \
+	--secret-permissions get
+
+# For each new certificate, create a cert on keyvault and add unversioned secret id to Application Gateway
+az keyvault certificate create \
+	--vault-name $vaultName \
+	-n mycert \
+	-p "$(az keyvault certificate get-default-policy)"
+
+
+## provisioning application gateway
+echo "provisioning application gateway"
+versionedSecretId=$(az keyvault certificate show -n mycert --vault-name $vaultName --query "sid" -o tsv)
+unversionedSecretId=$(echo $versionedSecretId | cut -d'/' -f-5) # remove the version from the url
 
 az group create --name $rgName --location $region
 
@@ -26,11 +65,14 @@ az network vnet create -n $vnetName -g $rgName --address-prefix 10.0.0.0/16 \
 az network application-gateway create -n $appgwName -l $region -g $rgName --sku Standard_v2 \
 	--public-ip-address $ipName --vnet-name $vnetName --subnet $subnetName --priority 100 \
 	--min-capacity 1 \
-  --http-settings-cookie-based-affinity Disabled \
-  --frontend-port 443 \
-  --http-settings-port 80 \
-  --http-settings-protocol Http \
-  --cert-file "C:\Users\Felix_Yu\Downloads\testCert.pfx" --cert-password "myTest@Pwd123" 
+	--http-settings-cookie-based-affinity Disabled \
+	--frontend-port 443 \
+	--http-settings-port 80 \
+	--http-settings-protocol Http \
+	--identity $identityID \
+	--ssl-certificate-name dlwkvsslcert \
+	--key-vault-secret-id $unversionedSecretId # ssl certificate with name "mykvsslcert" will be configured on AppGw
+  #--cert-file "C:\Users\Felix_Yu\Downloads\testCert.pfx" --cert-password "myTest@Pwd123" 
 
 ## provisioning aks
 echo "provisioning aks"

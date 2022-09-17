73a1b364d0995a815fa1bfbfef3c1c5f2fa40422
felixanna <yuecnu@hotmail.com>
2022-09-17 11:27:27 +0800

    update kind deployment scripts

--- deployment/kubernetes/kind/kind_provisioning.sh
+++ deployment/kubernetes/kind/kind_provisioning.sh
@@ -2,13 +2,11 @@ kind delete clusters dlw-cluster
 
 kind create cluster --config dlw-cluster.yml
 
+echo "install nginx (if you need kong, please uninstall nginx, then follow readme.md)"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
 echo "install metrics server"
 kubectl apply -f ../components/metrics/metrics.yaml
-
-echo "install nginx (if you need kong, please uninstall nginx, then follow readme.md)"
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
 echo "wait for nginx controller up before install services"
 kubectl wait --namespace ingress-nginx \

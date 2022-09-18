680f556e3828e00cb96a50c9fbef4ea20fafb95d
felixanna <yuecnu@hotmail.com>
2022-09-18 22:30:23 +0800

    update kind shell

--- deployment/kubernetes/kind/kind_provisioning.sh
+++ deployment/kubernetes/kind/kind_provisioning.sh
@@ -2,19 +2,20 @@ kind delete clusters dlw-cluster
 
 kind create cluster --config dlw-cluster.yml
 
-echo "install nginx (if you need kong, please uninstall nginx, then follow readme.md)"
+echo "install nginx  ..."
+echo "(if you need kong, please uninstall nginx, then follow readme.md)"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
-echo "install metrics server"
+echo "install metrics server ..."
 kubectl apply -f ../components/metrics/metrics.yaml
 
-echo "wait for nginx controller up before install services"
+echo "wait for nginx controller up before install services ..."
 kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=150s
 
-echo "install services"
+echo "install services ..."
 cd ..
 ns=dlwns
 helm upgrade --install dlw ./dlw-chart-nossl/ --namespace $ns --create-namespace --values ./dlw-chart-nossl/values_dev.yaml --wait
